00624cb88d1ece0debaf97738ff803035f2ece7e
Jacob Fitzhugh <j.rfitzhugh@gmail.com>
2019-12-18 04:39:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -108,6 +108,13 @@ class Student
   end
 
   def self.all_students_in_grade_X(grade_quarry)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade = ?
+    SQL
 
+    DB[:conn].execute(sql, grade_quarry).map do |row|
+      self.new_from_db(row)
+    end
   end
 end
